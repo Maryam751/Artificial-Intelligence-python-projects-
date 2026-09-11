78e3ba8afed23249169113144f0b79112f086e5a
Maryam751 <mrymmughal56@gmail.com>
2026-09-12 03:49:27 +0500

    Update lab1 and remove duplicate folder

--- 5_sem/lab1/alg1.py
+++ 5_sem/lab1/alg1.py
@@ -8,31 +8,3 @@ for i in range(len(A)):
 else:
     print('false')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-    A= [10,20,30,40]   
-t = int(input("Enter a number "))
-search = False
-for i in range(len(A)):
-    if A[i] == t:
-        search = True
-        break
-
-if search:
-    print(" number is in the list A ")    
-else:
-    print(" number is not in the lisst A")    
-    '''
